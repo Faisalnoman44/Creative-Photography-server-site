1a91b5a71ecefedd570aee668d0ac97049706d14
Faisalnoman44 <mdnoman.mn11@gmail.com>
2022-11-18 01:49:18 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -13,13 +14,41 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c0svav3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader?.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('photograpy').collection('services');
-        const commentCollection = client.db('photograpy').collection('comments')
-        const cameraCollection = client.db('photograpy').collection('cameras')
+        const commentCollection = client.db('photograpy').collection('comments');
+        const cameraCollection = client.db('photograpy').collection('cameras');
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ token })
+        })
 
         app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+        app.get('/allservices', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
@@ -37,7 +66,11 @@ async function run() {
 
         // comment api
 
-        app.get('/comment', async (req, res) => {
+        app.get('/comment', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' })
+            }
             let query = {}
             if (req.query.email) {
                 query = {
@@ -51,8 +84,6 @@ async function run() {
 
         app.get('/comments', async (req, res) => {
             let query = {}
-            console.log('faisal');
-            console.log(req.query.service)
             if (req.query.service) {
                 query = {
                     service: req.query.service
@@ -78,12 +109,19 @@ async function run() {
 
         // camera api
 
-        app.get('/camera', async(req, res) =>{
+        app.get('/camera', async (req, res) => {
             const query = {}
             const cursor = cameraCollection.find(query);
-            const cameras = await cursor.toArray()
-            res.send(cameras)
+            const camera = await cursor.toArray()
+            res.send(camera)
         })
+
+        // app.get('/camera', async(req, res) =>{
+        //     const query = {}
+        //     const cursor = cameraCollection.find(query);
+        //     const cameras = await cursor.toArray()
+        //     res.send(cameras)
+        // })
     }
     finally {
 
